578c7f46131db29f74fe873d65bacbf32af3eeb9
Evan Vreeland <evanavreeland@gmail.com>
2017-03-12 18:15:00 -0700

    Add edit page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,25 @@ class UserController < ApplicationController
   def show
     @user = User.find(1)
     @sports = ['baseball', 'soccer']
+    @user_sports = UserSports.where(user_id_id: 1)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :height, :weight)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #
   # root 'home#index'
   root 'user#show'
+  resources :user
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
